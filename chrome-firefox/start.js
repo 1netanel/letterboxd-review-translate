f3c58dc220979cea6240758c1759fc0cdef97843
Netanel Henya <natanelhenya@gmail.com>
2022-08-28 01:18:24 +0300

    detect language before transalteing every review

--- chrome-firefox/start.js
+++ chrome-firefox/start.js
@@ -1,40 +1,56 @@
-const sourceLang = 'auto';
-const targetLang = 'en';
+const sourceLang = "auto";
+const targetLang = "en";
+
+const detectLanguage = async (text) => {
+  // TODO: change from chrome to more generic option
+
+  const obj = await chrome.i18n.detectLanguage(text);
+  return obj.isReliable ? obj.languages[0].language : targetLang;
+};
 
 const fetchTranslation = async (textToTranslate) => {
-  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(textToTranslate)}`
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(
+    textToTranslate
+  )}`;
   const res = await fetch(url);
   const json = await res.json();
 
   const results = json[0];
-  const translatedText = results.map(line => line[0]).join()
+  const translatedText = results.map((line) => line[0]).join();
   return translatedText;
-}
+};
 
 const appendTranslatedReview = async (reviewTextDiv) => {
-  const reviewText = await reviewTextDiv.innerText
+  const reviewText = await reviewTextDiv.innerText;
   const translatedText = await fetchTranslation(reviewText);
-  reviewTextDiv.innerText = translatedText;
-}
+  // TODO: append translateion in a better way
+  reviewTextDiv.innerText += `\n\n${translatedText}\n`;
+};
 
 const createButton = () => {
-  const a = document.createElement('a');
+  const a = document.createElement("a");
   a.appendChild(document.createTextNode("Transalte Review"));
-  a.href = "javascript:void(0);"
+  a.href = "javascript:void(0);";
+  a.classList.add("js-reveal");
+  a.style.color = "#bcd";
+  a.style.fontFamily = "TiemposTextWeb-Regular, Georgia, serif";
   return a;
-}
+};
 
-const reviewList = document.querySelector(".viewings-list > ul");
-const reviews = reviewList.children;
+const main = async () => {
+  const reviews = document.getElementsByClassName("film-detail");
 
-const appendTransalteButtons = () => {
   for (const review of reviews) {
     const reviewTextDiv = review.querySelector(".body-text");
-    const a = createButton();
-    a.onclick = () => { appendTranslatedReview(reviewTextDiv) };
-    reviewTextDiv.after(a);
+    const reviewText = await reviewTextDiv.innerText;
+    const reviewLangauge = await detectLanguage(reviewText);
+
+    if (reviewLangauge !== targetLang) {
+      const a = createButton();
+      a.onclick = () => appendTranslatedReview(reviewTextDiv);
+      reviewTextDiv.after(a);
+    }
   }
-}
+};
 
-appendTransalteButtons()
-
+main();
